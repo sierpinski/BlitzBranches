1a44a74a56215eb460fb18486dea8ca5b01e3802
sierpinski <kylemccormick@outlook.com>
2020-12-19 16:29:07 -0800

    Added sorting and icon to indicate sortable.

--- scripts/blitzallbranches.js
+++ scripts/blitzallbranches.js
@@ -90,4 +90,18 @@ $(document).ready(function () {
 		$('tr:has(td:not(:contains(' + filterText + ')))').hide();
 		$('tr:has(td:contains(' + filterText + '))').show();
 	});
+	$('th').click(function(){
+		var table = $(this).parents('table').eq(0)
+		var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+		this.asc = !this.asc
+		if (!this.asc){rows = rows.reverse()}
+		for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+	})
+	function comparer(index) {
+		return function(a, b) {
+			var valA = getCellValue(a, index), valB = getCellValue(b, index)
+			return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+		}
+	}
+	function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
 });
